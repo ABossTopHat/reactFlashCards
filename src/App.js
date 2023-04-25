60bc98672015d974ec47f412113b873629cd0b72
Jacob Boss <125599727+ABossTopHat@users.noreply.github.com>
2023-04-25 16:01:15 -0700

    a commit, everything is working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Layout from "./Layout";
+import React,{useState, useEffect} from "react";
+import { Route, Switch, useRouteMatch} from "react-router-dom";
+import Layout from "./Home";
 import "./App.css";
-
+import Study from './Study'
+import View from './View'
+import Create from './CreateDeck'
+import { listDecks } from "./utils/api";
 /**
  * App is a wrapper for <Layout>, you should not need to change this file.
  */
 
 function App() {
+
+  const {url} = useRouteMatch()
   return (
     <div className="app-routes">
       <Switch>
-        <Route path="/">
+        <Route path='/create'>
+          <Create />
+        </Route>
+        <Route path="/" exact>
           <Layout />
+        </Route>
+        <Route path={`/decks/:deckId/study`} exact>
+          <Study />
+        </Route>
+        <Route path={`/decks/:deckId/view`}>
+          <View Layout={Layout()}/>
         </Route>
       </Switch>
     </div>
